c263f29f96c5f4520292b2c971a8e881d4510b6f
Matthias Petri <Matthias.Petri@gmail.com>
2017-05-17 16:06:34 +1000

    reorder algorithms for benchmark

--- benchmark.cpp
+++ benchmark.cpp
@@ -186,16 +186,6 @@ int main(int argc, char const* argv[])
         "col;part;method;postings;lists;size_bits;encoding_time_ns;"
         "decoding_time_ns\n");
 
-    run<ans_simple>(inputs.docids, out_prefix, col_name, "docids");
-    run<ans_simple>(inputs.freqs, out_prefix, col_name, "freqs");
-    run<ans_packed<128> >(inputs.docids, out_prefix, col_name, "docids");
-    run<ans_packed<128> >(inputs.freqs, out_prefix, col_name, "freqs");
-    run<ans_packed<256> >(inputs.docids, out_prefix, col_name, "docids");
-    run<ans_packed<256> >(inputs.freqs, out_prefix, col_name, "freqs");
-    run<ans_vbyte_split<4096> >(inputs.docids, out_prefix, col_name, "docids");
-    run<ans_vbyte_split<4096> >(inputs.freqs, out_prefix, col_name, "freqs");
-    run<ans_vbyte_single<4096> >(inputs.docids, out_prefix, col_name, "docids");
-    run<ans_vbyte_single<4096> >(inputs.freqs, out_prefix, col_name, "freqs");
     run<qmx>(inputs.docids, out_prefix, col_name, "docids");
     run<qmx>(inputs.freqs, out_prefix, col_name, "freqs");
     run<vbyte>(inputs.docids, out_prefix, col_name, "docids");
@@ -206,6 +196,17 @@ int main(int argc, char const* argv[])
     run<simple16>(inputs.freqs, out_prefix, col_name, "freqs");
     run<interpolative>(inputs.docids, out_prefix, col_name, "docids");
     run<interpolative>(inputs.freqs, out_prefix, col_name, "freqs");
+
+    run<ans_simple>(inputs.docids, out_prefix, col_name, "docids");
+    run<ans_simple>(inputs.freqs, out_prefix, col_name, "freqs");
+    run<ans_packed<128> >(inputs.docids, out_prefix, col_name, "docids");
+    run<ans_packed<128> >(inputs.freqs, out_prefix, col_name, "freqs");
+    run<ans_packed<256> >(inputs.docids, out_prefix, col_name, "docids");
+    run<ans_packed<256> >(inputs.freqs, out_prefix, col_name, "freqs");
+    run<ans_vbyte_split<4096> >(inputs.docids, out_prefix, col_name, "docids");
+    run<ans_vbyte_split<4096> >(inputs.freqs, out_prefix, col_name, "freqs");
+    run<ans_vbyte_single<4096> >(inputs.docids, out_prefix, col_name, "docids");
+    run<ans_vbyte_single<4096> >(inputs.freqs, out_prefix, col_name, "freqs");
 
     return EXIT_SUCCESS;
 }
